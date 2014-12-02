f2269dbc2d52d3ed5907186625a561ffd4714d2c
jupamedig <jpmendez@gmail.com>
2014-12-02 10:36:12 -0800

    added new form

--- medmento.js
+++ medmento.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
   bindListeners();
+
+  // (function(){
+  //   parseInt($(".frequency-period").val());
+  // });
 });
 
 function bindListeners(){
@@ -10,11 +14,20 @@ function bindListeners(){
 function createReminder(evt){
   evt.preventDefault();
   // var dataToSend = {}
+  // var frequency_period = parseInt($("#frequency-value").val());
+  var frequency_period = 5;
+  var patient_name = $("#patient-name").val();
+  var patient_number = $("#patient-number").val();
   var request = $.ajax({
     type: 'post',
     url: 'http://medmento.herokuapp.com/api/v1/events',
     dataType: 'json',
-    data: $(this).serialize()
+    // data: $(this).serialize()
+    data: {
+      patient_name: patient_name,
+      patient_number: patient_number,
+      frequency_period_id: frequency_period
+    }
   });
   request.done(function(res){
     console.log(res)
@@ -39,3 +52,15 @@ function getReminders(evt){
     console.log('get reminders fail!')
   });
 };
+
+
+// adds all the options to the frequency_quantity select dropdown in the form
+$(function(){
+    var $select = $(".1-31");
+    for (i=1;i<=31;i++){
+        $select.append($('<option></option>').val(i).html(i))
+    }
+});
+
+
+
