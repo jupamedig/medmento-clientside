5f53d91a79ef0ace4b74e6731964457d5c42bd10
johnlin1214 <johnlin1214@gmail.com>
2014-12-02 23:07:10 -0800

    added styling to headers and new events

--- medmento.js
+++ medmento.js
@@ -7,17 +7,18 @@ function bindListeners() {
   $("#reminder-form").on('submit', createReminder);
   $("#testing").on('click', getReminders);
   $(document).on('click', ".delete-event", deleteEvent);
+  dropDown();
 };
 
 function deleteEvent(evt) {
   evt.preventDefault();
   var id = $(this).closest('#med-row').data('event-id');
   $.ajax({
-  type: 'DELETE',
-  url: "http://medmento.herokuapp.com/api/v1/events/" + id
+    type: 'DELETE',
+    url: "http://medmento.herokuapp.com/api/v1/events/" + id
   }).done(function(){
 
-      var itemToRemove = $('div').filter(function(i,e) {
+    var itemToRemove = $('div').filter(function(i,e) {
       return $(e).data('event-id') == id;
     });
 
@@ -33,26 +34,35 @@ function loadEvents() {
     type: 'GET',
     url: 'http://medmento.herokuapp.com/api/v1/events'
   }).done(function(serverData){
-      for( i=0; i < serverData.length; i++ ) {
-        var drug_name = serverData[i].drug_name;
-        var patient_name =  serverData[i].patient_name;
-        var patient_number = serverData[i].patient_number;
-        var message = serverData[i].message;
-        var eventId = serverData[i].id;
+    for( i=0; i < serverData.length; i++ ) {
+      var drug_name = serverData[i].drug_name;
+      var patient_name =  serverData[i].patient_name;
+      var patient_number = serverData[i].patient_number;
+      var message = serverData[i].message;
+      var eventId = serverData[i].id;
 
-        var source = $("#person-template").html();
-        var template = Handlebars.compile(source);
-        var context = { drug_name: drug_name, patient_name: patient_name, patient_number: patient_number, message: message, eventId: eventId};
-
-        var html = template(context)
-        $("#account-reminders").prepend(html)
-      }
-      })
-    .fail(function(){console.log("failed initial load")})
+      var source = $("#person-template").html();
+      var template = Handlebars.compile(source);
+      var context = { drug_name: drug_name, patient_name: patient_name, patient_number: patient_number, message: message, eventId: eventId};
+      var html = template(context)
+      $("#account-reminders").prepend(html)
+    }
+  })
+  .fail(function(){console.log("failed initial load")})
 };
 
 function createReminder(evt) {
   evt.preventDefault();
+
+  var day = $("#day-of-week").val();
+  var time = $("#reminder-time").val();
+
+  if ($('#daily').is(':checked')) {
+    $("#at").val(time)
+  } else {
+    $("#at").val(day + " " + time)
+  }
+  data = $(this).serialize()
 
   var request = $.ajax({
     type: 'POST',
@@ -64,14 +74,12 @@ function createReminder(evt) {
     var source   = $("#person-template").html();
     var template = Handlebars.compile(source);
     var context = { drug_name: serverData.drug_name, patient_name: serverData.patient_name, patient_number: serverData.patient_name, message: serverData.message, eventId: serverData.id };
-    var html = template(context);
-
-    // var html = html.data("eventId", serverData)
+      var html = template(context);
     $("#account-reminders").prepend(html)
   });
   request.fail(function(res) {
     console.log('create reminder fail!')
-});
+  });
 };
 
 function getReminders(evt) {
@@ -89,43 +97,22 @@ function getReminders(evt) {
   });
 };
 
-// function getReminders(evt) {
-//   evt.preventDefault();
-//   var request = $.ajax({
-//     type: 'DELETE',
-//     url: 'http://medmento.herokuapp.com/api/v1/events'
-//   });
-//   request.done(function(res) {
-//     console.log(res)
-//   });
-//   request.fail(function(res) {
-//     console.log('get reminders fail!')
-//   });
-// };
-
-function deleteAll() {
-  for (var id = 0; id < 101; id ++) {
-    var request = $.ajax({
-      type: 'DELETE',
-      url: "http://medmento.herokuapp.com/api/v1/events/" + id
-    });
-    request.done(function(res) {
-      console.log(res)
-    });
-    request.fail(function(res) {
-      console.log('get reminders fail!')
-    });
-  }
+function dropDown() {
+  $('#weekly-dropdown').hide();
+  $('#weekly').on('click', function(){
+    $('#weekly-dropdown').fadeIn('slow');
+  });
+  $('#daily').on('click', function(){
+   $('#weekly-dropdown').hide('slow');
+ });
 }
 
 
-
-// adds all the options to the frequency_quantity select dropdown in the form
 $(function() {
-    var $select = $(".1-31");
-    for (i=1;i<=31;i++){
-        $select.append($('<option></option>').val(i).html(i))
-    }
+  var $select = $(".1-31");
+  for (i=1;i<=31;i++){
+    $select.append($('<option></option>').val(i).html(i))
+  }
 });
 
 
